69266fd5887258bd9d5544b2a9a475316d3308ab
nawaf awad <nawafawad@192.168.1.68>
2026-02-18 02:58:15 +0200

    0 minoperations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,19 +8,15 @@ def minOperations(n):
     """
     Calculates the fewest number of operations needed to result
     in exactly n H characters in the file.
-    
     Args:
         n (int): The target number of characters.
-        
     Returns:
         int: Sum of operations needed, or 0 if impossible.
     """
     if n <= 1:
         return 0
-    
     operations = 0
     divisor = 2
-    
     # Iterate to find prime factors
     while n > 1:
         # If n is divisible by divisor, divide it and add divisor to ops
@@ -28,6 +24,5 @@ def minOperations(n):
             operations += divisor
             n = n // divisor
         # Check the next number
-        divisor += 1
-        
+        divisor += 1   
     return operations
